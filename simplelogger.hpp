6035dabf4fa4d523e3b641942dea0fcfd8684500
Mark Fours 🐸 <tefnimdev@gmail.com>
2025-08-26 10:58:25 +0300

    -

---
+++ simplelogger.hpp
@@ -0,0 +1,29 @@
+/* ************
+* Purpose: Simple Logger
+* Author:  Mark 'EUL' Fours
+************ */
+
+#pragma once
+
+#include <iostream>
+#include <mutex>
+#include <chrono>
+#include <iomanip>
+#include <string>
+
+namespace simplelogger {
+
+// #TODO
+// Main Logic
+
+
+
+
+
+// Simple Logging
+// #define LOG_DEBUG(msg)   simplelogger::log(simplelogger::LogLevel::DEBUG, msg, __FILE__, __LINE__)
+// #define LOG_INFO(msg)    simplelogger::log(simplelogger::LogLevel::INFO, msg, __FILE__, __LINE__)
+// #define LOG_WARNING(msg) simplelogger::log(simplelogger::LogLevel::WARNING, msg, __FILE__, __LINE__)
+// #define LOG_ERROR(msg)   simplelogger::log(simplelogger::LogLevel::ERROR, msg, __FILE__, __LINE__)
+
+} // namespace simplelogger
