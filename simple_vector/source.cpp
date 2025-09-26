9f5e61f86868b51c6baf502571f262121fc34a3f
Mark Fours 🐸 <tefnimdev@gmail.com>
2025-09-26 07:22:07 +0300

    ref

---
+++ simple_vector/source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+
+int main(void) {
+
+// @todo
+// 123
+
+
+
+    // ==== Prevent the console from closing ====
+    std::cin.clear();
+    
+    std::cin.ignore(32767, '\n');
+    std::cin.get();
+    // ================
+    
+    
+    
+    return 0;
+}
